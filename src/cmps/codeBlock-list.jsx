db6c90be1e02c5f840744e02e6c8f33744d4a85b
carmel yona <carmelyona26@gmail.com>
2023-02-02 14:35:43 +0200

    mq for small device

--- src/cmps/codeBlock-list.jsx
+++ src/cmps/codeBlock-list.jsx
@@ -4,7 +4,7 @@ export const CodeBlockList = (props) => {
     const { codeBlocks, setCodeBlockId } = props
 
     return (
-        <section className="code-block-list flex ">
+        <section className="code-block-list flex">
             {codeBlocks.map(codeBlock => <CodeBlockPreview codeBlock={codeBlock} key={codeBlock._id} setCodeBlockId={setCodeBlockId} />)}
         </section>
     )
